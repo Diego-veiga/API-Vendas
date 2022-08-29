2ed554966def4be3fea17d2d0234a6f4e63bc8f2
Diego Veiga <diego.veiga@totvs.com.br>
2022-08-28 22:04:34 -0300

    t

--- src/modules/controllers/ProductControllers.ts
+++ src/modules/controllers/ProductControllers.ts
@@ -9,9 +9,9 @@ export default class ProductController {
   public async index(request: Request, response: Response): Promise<Response> {
     const listProducts = new ListProductService();
 
-    const productss = await listProducts.execute();
+    const productsss = await listProducts.execute();
 
-    return response.json(productss);
+    return response.json(productsss);
   }
 
   public async show(request: Request, response: Response): Promise<Response> {
